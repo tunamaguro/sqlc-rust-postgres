1afa16bffb9a9b3e0b4c1cb66657f75db090b795
tunamaguro <tsu7magu6@gmail.com>
2025-02-08 05:50:25 +0000

    chore: update sql
    Copy from https://github.com/cornucopia-rs/cornucopia/blob/d1229ae6948e691c40e851377b9e5a410305ec4f/examples/basic_async/schema.sql

--- query.sql
+++ query.sql
@@ -1,19 +1,32 @@
--- name: GetAuthor :one
-SELECT * FROM authors
-WHERE id = $1 LIMIT 1;
+
+-- name: GetAuthorByID :one
+SELECT id, name, country
+FROM Author
+WHERE id = $1;
+
+-- name: GetBookByID :one
+SELECT id, title, translations
+FROM Book
+WHERE id = $1;
 
 -- name: ListAuthors :many
-SELECT * FROM authors
+SELECT id, name, country
+FROM Author
 ORDER BY name;
 
--- name: CreateAuthor :one
-INSERT INTO authors (
-          name, bio
-) VALUES (
-  $1, $2
-)
-RETURNING *;
+-- name: ListBooks :many
+SELECT id, title, translations
+FROM Book
+ORDER BY id;
 
--- name: DeleteAuthor :exec
-DELETE FROM authors
-WHERE id = $1;
+-- name: CreateAuthor :exec
+INSERT INTO Author (name, country)
+VALUES ($1, $2);
+
+-- name: CreateBook :exec
+INSERT INTO Book (title)
+VALUES ($1);
+
+-- name: AssignAuthorToBook :exec
+INSERT INTO BookAuthor (AuthorId, BookId)
+VALUES ($1, $2);
--- schema.sql
+++ schema.sql
@@ -1,5 +1,48 @@
-CREATE TABLE authors (
-          id   BIGSERIAL PRIMARY KEY,
-          name text      NOT NULL,
-          bio  text
+CREATE TABLE Author (
+    Id serial NOT NULL,
+    Name varchar(70) NOT NULL,
+    Country varchar(100) NOT NULL,
+    PRIMARY KEY (Id)
 );
+
+INSERT INTO Author (Name, Country)
+    VALUES ('Agatha Christie', 'United Kingdom'), ('John Ronald Reuel Tolkien', 'United Kingdom');
+
+CREATE TABLE Book (
+    Id serial NOT NULL,
+    Title varchar(50) NOT NULL,
+    Translations text[] NOT NULL DEFAULT ARRAY['french', 'english'],
+    PRIMARY KEY (Id)
+);
+
+INSERT INTO Book (Title)
+    VALUES ('Murder on the Orient Express'), ('Death on the Nile'), ('The Hobbit'), ('The Silmarillion');
+
+CREATE TABLE BookAuthor (
+    AuthorId int NOT NULL,
+    BookId int NOT NULL,
+    FOREIGN KEY (AuthorId) REFERENCES Author (Id),
+    FOREIGN KEY (BookId) REFERENCES Book (Id)
+);
+
+INSERT INTO BookAuthor (AuthorId, BookId)
+    VALUES (1, 1), (1, 2), (2, 3), (2, 4);
+
+CREATE TYPE Sponge_Bob_Character AS enum (
+    'Bob',
+    'Patrick',
+    'Squidward'
+);
+
+CREATE TYPE VoiceActor AS (
+    name text,
+    age integer
+);
+
+CREATE TABLE SpongeBobVoiceActor (
+    voice_actor VoiceActor,
+    character Sponge_Bob_Character
+);
+
+INSERT INTO SpongeBobVoiceActor (voice_actor, character)
+    VALUES (ROW ('Bill Fagerbakke', 65), 'Patrick');
