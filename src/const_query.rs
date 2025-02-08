4536aa67bd2d07cd81eacdbae41fd30b98656168
tunamaguro <tsu7magu6@gmail.com>
2025-02-08 05:18:57 +0000

    refactor: to trait

--- src/const_query.rs
+++ src/const_query.rs
@@ -4,9 +4,11 @@ use proc_macro2::Span;
 use quote::{quote, ToTokens};
 use syn::Ident;
 
-trait FromQuery: Send + Sync + Sized {
-    type Error;
-    fn from_query(query: &plugin::Query) -> Result<Self, Self::Error>;
+pub(crate) trait GenericQuery {
+    // query ident name
+    fn ident_str(&self) -> String;
+    // sql query
+    fn sql_str(&self) -> String;
 }
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
@@ -15,11 +17,17 @@ pub(crate) struct PostgresQuery {
     query: String,
 }
 
-impl PostgresQuery {
-    pub(crate) fn ident_str(&self) -> String {
+impl GenericQuery for PostgresQuery {
+    fn ident_str(&self) -> String {
         self.name.to_case(Case::UpperSnake)
     }
 
+    fn sql_str(&self) -> String {
+        self.query.clone()
+    }
+}
+
+impl PostgresQuery {
     pub(crate) fn new(query: &plugin::Query) -> Self {
         Self {
             name: query.name.clone(),
@@ -31,7 +39,7 @@ impl PostgresQuery {
 impl ToTokens for PostgresQuery {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
         let ident = Ident::new(&self.ident_str(), Span::call_site());
-        let raw_str = format!("r#\"{}\"#", self.query);
+        let raw_str = format!("r#\"{}\"#", self.sql_str());
         let raw_literal: proc_macro2::TokenStream =
             raw_str.parse().expect("Failed to parse raw literal");
         tokens.extend(quote! {
